4b7a9f5c926588ffae06b5240c0443e9940af87b
Jim Gardner <jim88code@gmail.com>
2018-10-26 20:48:02 -0500

    Fetch and Display Repos

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = '12a1c667641fd385d707';
     this.client_secret = '640adb91018b2d1edde126f2b858d1a91c725b70';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
